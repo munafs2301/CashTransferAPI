cc9a996f7002ecd344b05aae57a60fd8497a9e72
munafs2301 <marvelousfrank5@gmail.com>
2021-05-22 11:40:21 +0100

    Add project files.

---
+++ Enitities/Balance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashTransferAPI.Enitities
+{
+    public class Balance
+    {
+        public int Id { get; set; }
+        public long AccountNumber { get; set; }
+        public double AccountBalance { get; set; }
+    }
+}
