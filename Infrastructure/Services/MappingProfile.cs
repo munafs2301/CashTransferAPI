52a48249ef88c37d6c68b6f03c4cf6bbc9dcd944
munafs2301 <marvelousfrank5@gmail.com>
2021-05-24 13:30:57 +0100

    Added Mapping Profile for Transaction

---
+++ Infrastructure/Services/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CashTransferAPI.Data.Models;
+using CashTransferAPI.Enitities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashTransferAPI.Infrastructure.Services
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Transaction, TransactionModel>()
+               .ForMember(c => c.AccountNumber, o => o.MapFrom(m => m.Balance.AccountNumber))
+               .ReverseMap();
+               //.ForMember(m => m.Balance, o => o.Ignore());
+        }
+
+    }
+}
