2aeb2bd814091c8c15e84401378056f4945c8c25
munafs2301 <marvelousfrank5@gmail.com>
2021-05-24 13:29:29 +0100

    Added Entities and Models

---
+++ Data/Enitities/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashTransferAPI.Enitities
+{
+    public class Transaction
+    {
+        [Key]
+        public Guid Reference { get; set; }
+        public double Amount { get; set; }
+        public int BeneficiaryAcccount { get; set; }
+        public Balance Balance { get; set; }
+    }
+}
---
+++ Data/Models/TransactionModel.cs
@@ -0,0 +1,22 @@
+﻿using CashTransferAPI.Enitities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashTransferAPI.Data.Models
+{
+    public class TransactionModel
+    {
+        public string Reference { get; set; }
+
+        [Required]
+        public double Amount { get; set; }
+        [Required]
+        public int BeneficiaryAccount { get; set; }
+        public int AccountNumber { get; set; }
+        public Balance Balance { get; set; }
+
+    }
+}
