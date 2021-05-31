a1158be4dcd20e6126248fb10fae81f93a1d66fa
munafs2301 <marvelousfrank5@gmail.com>
2021-05-31 14:54:09 +0100

    Added support for Xml content type

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,10 @@ namespace CashTransferAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+            }).AddXmlDataContractSerializerFormatters();
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddDbContext<CashTransferAPIContext>();
             services.AddScoped<ITransactionRepository, TransactionRepository>();
